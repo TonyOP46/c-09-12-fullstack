ac860d968d11271da93cb69b3dc317f5d04c86bd
TonyOP46 <tonymiguelo@gmail.com>
2023-03-05 22:09:42 -0500

    updated

--- src/utils/database.js
+++ src/utils/database.js
@@ -10,7 +10,7 @@ const db = new Sequelize({
     port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
-    dialectOptions: { ssl: { require: true, rejectUnauthorized: false }}
+    // dialectOptions: { ssl: { require: true, rejectUnauthorized: false }}
 
 
 })
